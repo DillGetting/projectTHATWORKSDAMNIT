752d738bae8b6ee37eb32fa7621dbeb2c7e8c9c7
Dillon Wilcox <83142241+DillGetting@users.noreply.github.com>
2021-12-08 00:42:58 -0700

    Create formtest.php
    page 266

---
+++ formtest.php
@@ -0,0 +1,16 @@
+<?php //formtest.php
+echo <<<_END
+<html>
+<head>
+<title>Trader</title>
+</head>
+<body>
+<form method="post" action="formtest.php">
+what is your genitalia?
+<input type="text" name="name">
+<input type="submit">
+</form>
+</body>
+</html>
+_END;
+?>
